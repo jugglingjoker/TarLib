f0c74d4b52f843f44d872439ba9ce812a2419e5b
Drew <jugglingjoker@gmail.com>
2025-05-03 17:13:54 -0600

    more spacing

--- Graphics/IDrawableBox.cs
+++ Graphics/IDrawableBox.cs
@@ -5,15 +5,12 @@ using TarLib.States;
 namespace TarLib.Graphics {
     public interface IDrawableBox {
         bool BoxDrawVisible { get; }
-
         Texture2D BoxDrawTexture { get; }
         Vector2 BoxDrawPosition { get; }
         float BoxDrawHeight { get; }
         float BoxDrawWidth { get; }
-
         Color BoxColor { get; }
         float BoxDrawDepth { get; }
-
         Color BoxBorderColor { get; }
         BorderSizeStyle BoxBorderSize { get; }
         float BoxBorderDrawDepth { get; }
--- Graphics/IDrawableCircle.cs
+++ Graphics/IDrawableCircle.cs
@@ -2,16 +2,13 @@
 using Microsoft.Xna.Framework.Graphics;
 
 namespace TarLib.Graphics {
-
     public interface IDrawableCircle {
         bool CircleDrawVisible { get; }
         Texture2D CircleDrawTexture { get; }
         Vector2 CircleDrawCenter { get; }
         float CircleDrawRadius { get; }
-
         Color CircleColor { get; }
         float CircleDrawDepth { get; }
-
         Color CircleBorderColor { get; }
         float CircleBorderWidth { get; }
         float CircleBorderDrawDepth { get; }
