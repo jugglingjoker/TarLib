6ab3d59a1da146be58460e75046a3ece42aceab2
Drew <jugglingjoker@gmail.com>
2025-05-04 16:06:46 -0600

    Renamed SimpleRotatingDrawableGameEntity to be more consistent with the other SimpleDrawableGameEntity models Simplified the implementation, moved most of it's capability one level up to SimpleDrawableGameEntity

---
+++ Entities/Drawable/SimpleDrawableGameEntityWithRotation.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using TarLib.Entities.Drawable;
+using TarLib.Extensions;
+using TarLib.Graphics;
+
+namespace TarLib.Entities {
+
+    public abstract class SimpleDrawableGameEntityWithRotation<TCommandTypesEnum, TStateTypesEnum, TAnimationManager> : SimpleDrawableGameEntity<TCommandTypesEnum, TStateTypesEnum, TAnimationManager>, IDrawableEntityWithRotatingAnimation<TStateTypesEnum>
+        where TCommandTypesEnum : Enum
+        where TStateTypesEnum : Enum
+        where TAnimationManager : IDrawableEntityAnimationManager {
+
+        public abstract int DrawAnimationRotation { get; }
+    }
+}
