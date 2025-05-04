ae6fbf972ba465893998254ec0f1d35103915c68
Drew <jugglingjoker@gmail.com>
2025-05-04 16:08:08 -0600

    SimpleDrawableGameEntities will now require definitions for most of the drawing variables. An AnimationManager handles which bounds of the sprite sheet to display based on the state of the Entity

--- Entities/Drawable/DrawableEntityAnimationManager.cs
+++ Entities/Drawable/DrawableEntityAnimationManager.cs
@@ -2,15 +2,21 @@
 using System;
 
 namespace TarLib.Entities.Drawable {
-    public abstract class DrawableEntityAnimationManager<TEntity, TStateTypesEnum>
+
+    public interface IDrawableEntityAnimationManager {
+        Rectangle? Bounds { get; }
+        void Update(float elapsedTime);
+    }
+
+    public abstract class DrawableEntityAnimationManager<TEntity, TStateTypesEnum> : IDrawableEntityAnimationManager
         where TEntity : IDrawableEntityWithAnimation<TStateTypesEnum>
         where TStateTypesEnum : Enum {
 
         public TEntity Entity { get; }
 
-        public DrawableEntityAnimation<TEntity> CurrentAnimation => this[Entity.State.Type] ?? DefaultAnimation;
-        protected abstract DrawableEntityAnimation<TEntity> DefaultAnimation { get; }
-        protected abstract DrawableEntityAnimation<TEntity> this[TStateTypesEnum state] { get; }
+        public IDrawableEntityAnimation CurrentAnimation => this[Entity.State.Type] ?? DefaultAnimation;
+        protected abstract IDrawableEntityAnimation DefaultAnimation { get; }
+        protected abstract IDrawableEntityAnimation this[TStateTypesEnum state] { get; }
 
         public Rectangle? Bounds => new Rectangle(
             x: (CurrentAnimation?.FrameX ?? 0) * Entity.DrawAnimationFrameWidth,
@@ -23,16 +29,16 @@ namespace TarLib.Entities.Drawable {
             Entity.OnStateChange += OnEntityStateChange;
         }
 
-        public void OnEntityStateChange(object sender, (IEntityState<TStateTypesEnum> oldState, IEntityState<TStateTypesEnum> newState) stateChangeData) {
+        public void Update(float elapsedTime) {
+            CurrentAnimation.Update(elapsedTime);
+        }
+
+        private void OnEntityStateChange(object sender, (IEntityState<TStateTypesEnum> oldState, IEntityState<TStateTypesEnum> newState) stateChangeData) {
             var oldAnimation = this[stateChangeData.oldState.Type];
             var newAnimation = this[stateChangeData.newState.Type];
             if (oldAnimation != newAnimation) {
                 newAnimation?.Reset();
             }
-        }
-
-        public void Update(float elapsedTime) {
-            CurrentAnimation.Update(elapsedTime);
         }
     }
 }
--- Entities/Drawable/SimpleDrawableGameEntity.cs
+++ Entities/Drawable/SimpleDrawableGameEntity.cs
@@ -3,20 +3,64 @@ using Microsoft.Xna.Framework.Graphics;
 using System;
 using TarLib.Entities.Drawable;
 using TarLib.Extensions;
+using TarLib.Graphics;
 
 namespace TarLib.Entities {
-    public abstract class SimpleDrawableGameEntity<TCommandTypesEnum, TStateTypesEnum> : SimpleGameEntity<TCommandTypesEnum, TStateTypesEnum>, IDrawableEntity
+
+    public abstract class SimpleDrawableGameEntity<TCommandTypesEnum, TStateTypesEnum, TAnimationManager> : SimpleGameEntity<TCommandTypesEnum, TStateTypesEnum>, IDrawableEntityWithAnimation<TStateTypesEnum>
         where TCommandTypesEnum : Enum
-        where TStateTypesEnum : Enum {
+        where TStateTypesEnum : Enum
+        where TAnimationManager : IDrawableEntityAnimationManager {
 
-        public IDrawableEntity Drawable { get; }
+        public abstract Texture2D DrawTexture { get; }
+        public abstract Vector2 DrawOrigin { get; }
+        public abstract float DrawRotation { get; }
+        public abstract Vector2 DrawScale { get; }
+        public abstract SpriteEffects DrawEffects { get; }
+        public abstract Color DrawColor { get; }
+        public abstract Vector2 DrawPosition { get; }
+        public abstract float DrawDepth { get; }
+        public abstract bool DrawVisible { get; }
 
-        public float DrawWidth => Drawable.DrawWidth;
-        public float DrawHeight => Drawable.DrawHeight;
-        public Vector2 DrawPosition => Drawable.DrawPosition;
+        public abstract int DrawAnimationFrameWidth { get; }
+        public abstract int DrawAnimationFrameHeight { get; }
 
-        public void Draw(GameTime gameTime, SpriteBatch spriteBatch, Vector2 position = default, float startDepth = 0, float endDepth = 1) {
-            Drawable.Draw(gameTime, spriteBatch, position, startDepth, endDepth);
+        public virtual float DrawWidth => DrawAnimationFrameWidth;
+        public virtual float DrawHeight => DrawAnimationFrameHeight;
+
+        public TAnimationManager AnimationManager { get; protected set; }
+        private Texture texture;
+
+        public SimpleDrawableGameEntity() {
+            texture = new(this);
+        }
+
+        public override void Update(float elapsedTime) {
+            base.Update(elapsedTime);
+            AnimationManager.Update(elapsedTime);
+        }
+
+        public virtual void Draw(GameTime gameTime, SpriteBatch spriteBatch, Vector2 positionOffset = default, float startDepth = 0, float endDepth = 1) {
+            spriteBatch.Draw(texture, positionOffset, startDepth, endDepth);
+        }
+
+        private class Texture : IDrawableTexture {
+            public Texture2D DrawTexture => Entity.DrawTexture;
+            public Rectangle? DrawTextureFrame => Entity.AnimationManager.Bounds;
+            public Vector2 DrawOrigin => Entity.DrawOrigin;
+            public float DrawRotation => Entity.DrawRotation;
+            public Vector2 DrawScale => Entity.DrawScale;
+            public SpriteEffects DrawEffects => Entity.DrawEffects;
+            public Color DrawColor => Entity.DrawColor;
+            public Vector2 DrawPosition => Entity.DrawPosition;
+            public float DrawDepth => Entity.DrawDepth;
+            public bool DrawVisible => Entity.DrawVisible;
+
+            public SimpleDrawableGameEntity<TCommandTypesEnum, TStateTypesEnum, TAnimationManager> Entity { get; }
+
+            public Texture(SimpleDrawableGameEntity<TCommandTypesEnum, TStateTypesEnum, TAnimationManager> entity) {
+                Entity = entity;
+            }
         }
     }
 }
