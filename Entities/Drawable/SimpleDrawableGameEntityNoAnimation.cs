ac74e12365d8d746096a903e977962b560fb1f82
Drew <jugglingjoker@gmail.com>
2025-05-04 16:05:13 -0600

    Added a generic version of SimpleDrawableGameEntity for entities that will not have any animation

---
+++ Entities/Drawable/SimpleDrawableGameEntityNoAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TarLib.Entities.Drawable;
+
+namespace TarLib.Entities {
+    public abstract class SimpleDrawableGameEntityNoAnimation<TCommandTypesEnum, TStateTypesEnum> : SimpleDrawableGameEntity<TCommandTypesEnum, TStateTypesEnum, SimpleDrawableGameEntityNoAnimation<TCommandTypesEnum, TStateTypesEnum>.NoAnimationAnimationManager>
+        where TCommandTypesEnum : Enum
+        where TStateTypesEnum : Enum {
+
+        public override int DrawAnimationFrameWidth => DrawTexture.Width;
+        public override int DrawAnimationFrameHeight => DrawTexture.Height;
+
+        public SimpleDrawableGameEntityNoAnimation() {
+            AnimationManager = new NoAnimationAnimationManager(this);
+        }
+
+        public class NoAnimationAnimationManager : DrawableEntityAnimationManager<SimpleDrawableGameEntity<TCommandTypesEnum, TStateTypesEnum, NoAnimationAnimationManager>, TStateTypesEnum> {
+            private readonly NoAnimation noAnimation;
+
+            public NoAnimationAnimationManager(SimpleDrawableGameEntityNoAnimation<TCommandTypesEnum, TStateTypesEnum> entity) : base(entity) {
+                noAnimation = new NoAnimation();
+            }
+
+            protected override IDrawableEntityAnimation this[TStateTypesEnum state] => noAnimation;
+            protected override IDrawableEntityAnimation DefaultAnimation => noAnimation;
+
+            private class NoAnimation : IDrawableEntityAnimation {
+                public NoAnimation() {
+                    
+                }
+
+                public int FrameX => 0;
+                public int FrameY => 0;
+                public void Reset() { }
+                public void Update(float elapsedTime) { }
+            }
+        }
+    }
+}
